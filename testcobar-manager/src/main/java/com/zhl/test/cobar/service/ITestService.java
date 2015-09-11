064c35940b1e76158d4df14f95f61dc9e10617f9
hardrocksman <zhenghl0517@163.com>
2015-09-11 17:42:54 +0800

    cobar init

---
+++ testcobar-manager/src/main/java/com/zhl/test/cobar/service/ITestService.java
@@ -0,0 +1,9 @@
+package com.zhl.test.cobar.service;
+
+/**
+ * Created by Administrator on 2015/9/11.
+ */
+public interface ITestService {
+
+    public Long insert();
+}
---
+++ testcobar-manager/src/main/java/com/zhl/test/cobar/service/impl/TestService.java
@@ -0,0 +1,14 @@
+package com.zhl.test.cobar.service.impl;
+
+import com.zhl.test.cobar.service.ITestService;
+
+/**
+ * Created by Administrator on 2015/9/11.
+ */
+public class TestService implements ITestService{
+
+    @Override
+    public Long insert() {
+        return null;
+    }
+}
